78acdbf2ef27a61549e7593040bc030167922748
andrew <andrew_rich@hotmail.com>
2016-11-09 17:26:26 -0500

    refined code, added button style

--- app.js
+++ app.js
@@ -1,56 +1,32 @@
 $(document).ready(function(){
-// $('#add').click(function(){
-//     var text = $('input').val() + '  <button>x</button>';
-//     if(text.length){
-//         $('<li />', {html: text}).appendTo('ol.shoppinglist');
-//     }
-//     // clears the input field when add button clicked
-//     $("input").val("");  
-// });
+	
+	//click button, add input to list
+	$('#add').click(function() {
+	   $('ol').append('<li>' + $('input').val() + '   <button id="remove" >x</button></li>');
+	   $("input").val("");
+	});
 
 
-// // this removes the added list item by clicking the x
-// $('ol').on('click','button' , function(el){
-//     $(this).parent().remove();
-// });
+	//click the x to remove
+	$('ol').on('click', 'button', function() {
+	   $(this).parent().remove();
+	});
 
 
-// // trying to line-through when li is clicked NOT WORKING
-//  $("li").click(function() {
-//         $(this).toggleClass(".test");
-//     });
-
-/*features i want to add
-1.enter adds text to list from imput field
-2. dropdown menu for general items
-3. line-through when a li is clicked, 
-and remved when clicked again
-*/
-$('#add').click(function() {
-   $('ol').append('<li>' + $('input').val() + '   <button>x</button></li>');
-   $("input").val("");
-});
+	// click on li to create a line through
+	$("ol").on("click", "li", function() {
+	  $(this).toggleClass("test");
+	});
 
 
-$('ol').on('click', 'button', function() {
-   $(this).parent().remove();
-});
-
-
-// click on li to create a line through
-$("ol").on("click", "li", function() {
-  $(this).toggleClass("test");
-
-});
-
-// add txt to input press enter to add to list
-$("input").on("keypress",function(event){
-    if(event.which === 13){
-  $('ol').append('<li>' + $('input').val() + '   <button>x</button></li>');
-   $("input").val("");
-  $("p").remove();
-  }
-});
+	// add txt to input press enter to add to list
+	$("input").on("keypress",function(event){
+	    if(event.which === 13){
+	  		$('ol').append('<li>' + $('input').val() + '   <button id="remove" >x</button></li>');
+	   		$("input").val("");
+	  		$("p").remove();
+	  	}
+	});
 
 
 
